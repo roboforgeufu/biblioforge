5db70e1ce5aad1f8ccce3bb79a87f3910a941ec8
me15degrees <me15degrees@gmail.com>
2024-10-11 14:35:55 -0300

    feat: add new functions without obstacle handling

--- Path Finding/Python/src/dijkstra.py
+++ Path Finding/Python/src/dijkstra.py
@@ -6,35 +6,50 @@ ou seja, não existe vértice que volta do estabelecimento pela linha amarela. D
 Todos os pesos estão unitários, apenas na inicialização que as distâncias são tratadas como infinitas (característica do algoritmo).
 """
 
-#TODO fazer tratativa para obstáculos
-#TODO fazer com que o obstáculo seja desmarcado quando eu não consigo achar mais o caminho
-#TODO colocar pesos como distâncias 
+# TODO fazer tratativa para obstáculos
+# TODO fazer com que o obstáculo seja desmarcado quando eu não consigo achar mais o caminho
+
+TAM_ROBO_DIVIDIDO_POR_DOIS = 7.7
 
 class Grafo:
-    def __init__(self, matriz_lista_adjacencia):
+    def __init__(self, matriz_lista_adjacencia, matriz_obstaculos):
         self.num_vertices = len(matriz_lista_adjacencia)
         self.matriz_adj = matriz_lista_adjacencia
-    
+        self.matriz_obs = matriz_obstaculos
+        self.obstaculos = []
+        self.valores_antigos = {}  
+
     def adicionar_arestas(self, origem, destino, direcao, peso=1):
-        self.matriz_adj[origem].append((f"V{destino}", f"{direcao}", peso)) 
-    
+        self.matriz_adj[origem].append([f"V{destino}", f"{direcao}", peso])
+
     def remover_arestas(self, origem, destino):
         vertice = "V" + str(destino)
         for indice, items in enumerate(self.matriz_adj[origem]):
-            primeiro_item= items[0]
-            print(primeiro_item)
+            primeiro_item = items[0]
             if primeiro_item == vertice:
+                print(self.matriz_adj[origem])
                 self.matriz_adj[origem].pop(indice)
 
-    def mostrar_matriz(self):
-        for linha in self.matriz_adj:
+    def mostrar_matriz(self, matriz):
+        for linha in matriz:
             print(linha)
+
+    def marcar_obstaculo(self, vertice):
+        for linha in self.matriz_obs:
+            for aresta in linha:
+                if str(aresta[0]).strip() == vertice:
+                    aresta[2] = 100000
+                    self.obstaculos.append([vertice, aresta])
+
+    def desmarcar_obstaculo(self, vertice):
+        ...
+
 
     def dijkstra(self, inicio, fim):
         distancias = {i: float('inf') for i in range(self.num_vertices)}
         distancias[inicio] = 0
       
-        heap = [(0, inicio)]  # (distancia, vertice)
+        heap = [(0, inicio)]  
         
         anterior = {i: None for i in range(self.num_vertices)}
         direcao = {i: None for i in range(self.num_vertices)}
@@ -49,21 +64,22 @@ class Grafo:
             if dist_atual > distancias[vertice_atual]:
                 continue
 
-            # Verifica todos os vizinhos
             for vizinho_info in self.matriz_adj[vertice_atual]:
-                vizinho, dir_cardinal, peso = vizinho_info  # Extrai vértice, direção e peso
-                vizinho = int(vizinho[1:])  
+                vizinho, dir_cardinal, peso = vizinho_info  
+                vizinho_num = int(vizinho[1:])  
 
-                nova_distancia = dist_atual + peso  # Soma o peso da aresta
+                if vizinho in self.obstaculos:  
+                    continue
 
-                if nova_distancia < distancias[vizinho]:
-                    distancias[vizinho] = nova_distancia
-                    anterior[vizinho] = vertice_atual
-                    direcao[vizinho] = dir_cardinal  # Salva a direção
-                    pesos[vizinho] = peso  # Salva o peso da aresta
-                    heapq.heappush(heap, (nova_distancia, vizinho))
+                nova_distancia = dist_atual + peso  
 
-        # Reconstrói o caminho
+                if nova_distancia < distancias[vizinho_num]:
+                    distancias[vizinho_num] = nova_distancia
+                    anterior[vizinho_num] = vertice_atual
+                    direcao[vizinho_num] = dir_cardinal 
+                    pesos[vizinho_num] = peso  
+                    heapq.heappush(heap, (nova_distancia, vizinho_num))
+
         caminho = []
         direcoes_pesos = []  
         if distancias[fim] != float('inf'):
@@ -78,60 +94,116 @@ class Grafo:
 
         return caminho, distancias[fim], direcoes_pesos
 
-# Matriz de lista de adjacências 
+    def recalcular_caminho_sem_obstaculos(self, inicio, fim):
+        for vertice in self.obstaculos:
+            self.desmarcar_obstaculo(vertice)  
+            caminho, distancia, _ = self.dijkstra(inicio, fim)
+            if distancia != float('inf'):  
+                return caminho, distancia
+        return None, float('inf')  
+
 matriz_lista_adjacencia = [
     [],
-    [("V0", "O", 1), ("V2", "L", 1), ("V7", "S", 1)],
+    [["V0", "O", 24], ["V2", "L", 30], ["V7", "S", 30]],
     [],
-    [("V4", "L", 1), ("V9", "S", 1)],
+    [["V4", "L", 30], ["V9", "S", 30]],
     [],
-    [("V6", "L", 1), ("V11", "S", 1)],
-    [("V5", "O", 1)],
-    [("V1", "N", 1), ("V8", "L", 1), ("V14", "S", 1)],
-    [("V7", "O", 1), ("V2", "N", 1), ("V9", "L", 1)],
-    [("V8", "O", 1), ("V3", "N", 1), ("V10", "L", 1), ("V16", "S", 1)],
-    [("V9", "O", 1), ("V4", "N", 1), ("V17", "S", 1), ("V11", "L", 1)],
-    [("V10", "O", 1), ("V12", "L", 1), ("V5", "N", 1), ("V18", "S", 1)],
-    [("V11", "O", 1)],
+    [["V6", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V11", "S", 30]],
+    [["V5", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [["V1", "N", 30], ["V8", "L", 30], ["V14", "S", 30]],
+    [["V7", "O", 30], ["V2", "N", 30], ["V9", "L", 30]],
+    [["V8", "O", 30], ["V3", "N", 30], ["V10", "L", 30], ["V16", "S", 30]],
+    [["V9", "O", 30], ["V4", "N", 30], ["V17", "S", 30], ["V11", "L", 30]],
+    [["V10", "O", 1], ["V12", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V5", "N", 30], ["V18", "S", 30]],
+    [["V11", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
     [],
-    [("V13", "O", 1), ("V15", "L", 1), ("V20", "S", 1), ("V7", "N", 1)],
+    [["V13", "O", 24], ["V15", "L", 30], ["V20", "S", 30], ["V7", "N", 30]],
     [],
-    [("V15", "O", 1), ("V9", "N", 1), ("V22", "S", 1)],
+    [["V15", "O", 30], ["V9", "N", 30], ["V22", "S", 30]],
     [],
-    [("V11", "N", 1), ("V19", "L", 1), ("V24", "S", 1)],
-    [("V18", "O", 1)],
-    [("V14", "N", 1), ("V21", "L", 1), ("V27", "S", 1)],
-    [("V28", "S", 1), ("V22", "L", 1),("V20","O",1)],
-    [("V21", "O", 1), ("V23", "L", 1), ("V29", "S", 1),("V16","N",1)],
-    [("V22", "O", 1), ("V17", "N", 1), ("V30", "S", 1), ("V24", "L", 1)],
-    [("V23", "O", 1), ("V25", "L", 1), ("V31", "S", 1),("V18","N",1)],
-    [("V24", "O", 1)],
+    [["V11", "N", 30], ["V19", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "S", 30]],
+    [["V18", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [["V14", "N", 30], ["V21", "L", 30], ["V27", "S", 30]],
+    [["V28", "S", 30], ["V22", "L", 30], ["V20", "O", 30]],
+    [["V21", "O", 30], ["V23", "L", 30], ["V29", "S", 30], ["V16", "N", 30]],
+    [["V22", "O", 30], ["V17", "N", 30], ["V30", "S", 30], ["V24", "L", 30]],
+    [["V23", "O", 30], ["V25", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V31", "S", 30], ["V18", "N", 30]],
+    [["V24", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
     [],
-    [("V26", "O", 1), ("V20", "N", 1)],
+    [["V26", "O", 24], ["V20", "N", 30]],
     [],
-    [("V28", "O", 1), ("V22", "N", 1)],
+    [["V28", "O", 30], ["V22", "N", 30]],
     [],
-    [("V30", "O", 1), ("V32", "L", 1), ("V24", "N", 1)],
-    [("V31", "O", 1)]
+    [["V30", "O", 30], ["V32", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "N", 30]],
+    [["V31", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
 ]
 
-def main():
+matriz_obstaculos = [
+    [1],
+    [["V0", "O", 24], ["V2", "L", 30], ["V7", "S", 30]],
+    [],
+    [["V4", "L", 30], ["V9", "S", 30]],
+    [],
+    [["V6", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V11", "S", 30]],
+    [["V5", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [["V1", "N", 30], ["V8", "L", 30], ["V14", "S", 30]],
+    [["V7", "O", 30], ["V2", "N", 30], ["V9", "L", 30]],
+    [["V8", "O", 30], ["V3", "N", 30], ["V10", "L", 30], ["V16", "S", 30]],
+    [["V9", "O", 30], ["V4", "N", 30], ["V17", "S", 30], ["V11", "L", 30]],
+    [["V10", "O", 1], ["V12", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V5", "N", 30], ["V18", "S", 30]],
+    [["V11", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [],
+    [["V13", "O", 24], ["V15", "L", 30], ["V20", "S", 30], ["V7", "N", 30]],
+    [],
+    [["V15", "O", 30], ["V9", "N", 30], ["V22", "S", 30]],
+    [],
+    [["V11", "N", 30], ["V19", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "S", 30]],
+    [["V18", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [["V14", "N", 30], ["V21", "L", 30], ["V27", "S", 30]],
+    [["V28", "S", 30], ["V22", "L", 30], ["V20", "O", 30]],
+    [["V21", "O", 30], ["V23", "L", 30], ["V29", "S", 30], ["V16", "N", 30]],
+    [["V22", "O", 30], ["V17", "N", 30], ["V30", "S", 30], ["V24", "L", 30]],
+    [["V23", "O", 30], ["V25", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V31", "S", 30], ["V18", "N", 30]],
+    [["V24", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [],
+    [["V26", "O", 24], ["V20", "N", 30]],
+    [],
+    [["V28", "O", 30], ["V22", "N", 30]],
+    [],
+    [["V30", "O", 30], ["V32", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "N", 30]],
+    [["V31", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]]
+]
 
-    # Exemplo de início e fim, mas poderia ser qualquer outro input
+
+matriz_obstaculos = matriz_lista_adjacencia
+
+def main():
+    
     inicio = 20
     fim = 6
 
-    grafo = Grafo(matriz_lista_adjacencia)
+    grafo = Grafo(matriz_lista_adjacencia,matriz_obstaculos)
 
-    caminho, distancia, direcoes_pesos = grafo.dijkstra(inicio, fim)
 
+    grafo.marcar_obstaculo("V5")
+    grafo.mostrar_matriz(matriz_obstaculos)
+
+    caminho, distancia, direcoes_pesos = grafo.dijkstra(inicio,fim)
+    
     if distancia != float('inf'):
+        print()
         print(f"Os vértices visitados são: {caminho}")
         print(f"A distância mínima calculada vale: {distancia}")
         print(f"A saída que o Zé pediu: {direcoes_pesos}")
-
     else:
-        print(f"Não há caminho entre {inicio} e {fim}")
-
-
+       
+        print("Caminho bloqueado, tentando remover obstáculos.")
+        caminho, distancia = grafo.recalcular_caminho_sem_obstaculos(inicio, fim)
+        if distancia != float('inf'):
+            print(f"Caminho após remover obstáculos: {caminho}")
+            print(f"A distância mínima recalculada vale: {distancia}")
+        else:
+            print("Não foi possível encontrar um caminho, mesmo sem os obstáculos.")
+    
+    
 main()
--- Path Finding/Python/tests/test_dijkstra.py
+++ Path Finding/Python/tests/test_dijkstra.py
@@ -1,60 +1,68 @@
-from src.dijkstra import Grafo
+from src.dijkstra import Grafo, TAM_ROBO_DIVIDIDO_POR_DOIS, matriz_lista_adjacencia, matriz_obstaculos
 
-matriz_lista_adjacencia = [
-    [],
-    [("V0", "O", 1), ("V2", "L", 1), ("V7", "S", 1)],
-    [],
-    [("V4", "L", 1), ("V9", "S", 1)],
-    [],
-    [("V6", "L", 1), ("V11", "S", 1)],
-    [("V5", "O", 1)],
-    [("V1", "N", 1), ("V8", "L", 1), ("V14", "S", 1)],
-    [("V7", "O", 1), ("V2", "N", 1), ("V9", "L", 1)],
-    [("V8", "O", 1), ("V3", "N", 1), ("V10", "L", 1), ("V16", "S", 1)],
-    [("V9", "O", 1), ("V4", "N", 1), ("V17", "S", 1), ("V11", "L", 1)],
-    [("V10", "O", 1), ("V12", "L", 1), ("V5", "N", 1), ("V18", "S", 1)],
-    [("V11", "O", 1)],
-    [],
-    [("V13", "O", 1), ("V15", "L", 1), ("V20", "S", 1), ("V7", "N", 1)],
-    [],
-    [("V15", "O", 1), ("V9", "N", 1), ("V22", "S", 1)],
-    [],
-    [("V11", "N", 1), ("V19", "L", 1), ("V24", "S", 1)],
-    [("V18", "O", 1)],
-    [("V14", "N", 1), ("V21", "L", 1), ("V27", "S", 1)],
-    [("V28", "S", 1), ("V22", "L", 1),("V20","O",1)],
-    [("V21", "O", 1), ("V23", "L", 1), ("V29", "S", 1),("V16","N",1)],
-    [("V22", "O", 1), ("V17", "N", 1), ("V30", "S", 1), ("V24", "L", 1)],
-    [("V23", "O", 1), ("V25", "L", 1), ("V31", "S", 1),("V18","N",1)],
-    [("V24", "O", 1)],
-    [],
-    [("V26", "O", 1), ("V20", "N", 1)],
-    [],
-    [("V28", "O", 1), ("V22", "N", 1)],
-    [],
-    [("V30", "O", 1), ("V32", "L", 1), ("V24", "N", 1)],
-    [("V31", "O", 1)]
-]
-
-def test_dijkstra_menor_caminho():
-    grafo = Grafo(matriz_lista_adjacencia)
+def test_dijkstra():
+    grafo = Grafo(matriz_lista_adjacencia, matriz_obstaculos)
     caminho, distancia, direcoes = grafo.dijkstra(1, 9)
     assert caminho == [1, 7, 8, 9]
-
-def test_mostrar_matriz(capsys):
-    grafo = Grafo(matriz_lista_adjacencia)
-    grafo.mostrar_matriz()
-    captured = capsys.readouterr()
-    assert "('V0', 'O', 1)" in captured.out
-    assert "('V11', 'S', 1)" in captured.out
-
+    
 def test_adicionar_arestas():
-    grafo = Grafo(matriz_lista_adjacencia)
+    grafo = Grafo(matriz_lista_adjacencia, matriz_obstaculos)
     grafo.adicionar_arestas(10, 0, "N", 2)
-    assert grafo.matriz_adj[10] == [("V9", "O", 1), ("V4", "N", 1), ("V17", "S", 1), ("V11", "L", 1),("V0", "N", 2)] # Adiciona ao final da lista por conta do append
+    assert grafo.matriz_adj[10] == [['V9', 'O', 30], ['V4', 'N', 30], ['V17', 'S', 30], ['V11', 'L', 30], ['V0', 'N', 2]]
 
 def test_remover_arestas():
-    grafo = Grafo(matriz_lista_adjacencia)
+    grafo = Grafo(matriz_lista_adjacencia, matriz_obstaculos)
     grafo.remover_arestas(8,2)
-    assert grafo.matriz_adj[8] == [("V7", "O", 1), ("V9", "L", 1)]
+    print(grafo.matriz_adj)
+    assert grafo.matriz_adj[8] == [["V7", "O", 30], ["V9", "L", 30]]
+    grafo.matriz_adj.insert(2, [2,"N",30])
+    
 
+def test_marcar_obstaculo():
+    grafo = Grafo(matriz_lista_adjacencia, matriz_obstaculos)
+    grafo.marcar_obstaculo("V5")   
+    matriz_esperada = [
+    [],
+    [["V0", "O", 24], ["V2", "L", 30], ["V7", "S", 30]],
+    [],
+    [["V4", "L", 30], ["V9", "S", 30]],
+    [],
+    [["V6", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V11", "S", 30]],
+    [["V5", "O", 100000]],
+    [["V1", "N", 30], ["V8", "L", 30], ["V14", "S", 30]],
+    [["V7", "O", 30], ["V2", "N", 30], ["V9", "L", 30]],
+    [["V8", "O", 30], ["V3", "N", 30], ["V10", "L", 30], ["V16", "S", 30]],
+    [["V9", "O", 30], ["V4", "N", 30], ["V17", "S", 30], ["V11", "L", 30]],
+    [["V10", "O", 1], ["V12", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V5", "N", 100000], ["V18", "S", 30]],
+    [["V11", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [],
+    [["V13", "O", 24], ["V15", "L", 30], ["V20", "S", 30], ["V7", "N", 30]],
+    [],
+    [["V15", "O", 30], ["V9", "N", 30], ["V22", "S", 30]],
+    [],
+    [["V11", "N", 30], ["V19", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "S", 30]],
+    [["V18", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [["V14", "N", 30], ["V21", "L", 30], ["V27", "S", 30]],
+    [["V28", "S", 30], ["V22", "L", 30], ["V20", "O", 30]],
+    [["V21", "O", 30], ["V23", "L", 30], ["V29", "S", 30], ["V16", "N", 30]],
+    [["V22", "O", 30], ["V17", "N", 30], ["V30", "S", 30], ["V24", "L", 30]],
+    [["V23", "O", 30], ["V25", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V31", "S", 30], ["V18", "N", 30]],
+    [["V24", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+    [],
+    [["V26", "O", 24], ["V20", "N", 30]],
+    [],
+    [["V28", "O", 30], ["V22", "N", 30]],
+    [],
+    [["V30", "O", 30], ["V32", "L", 18-TAM_ROBO_DIVIDIDO_POR_DOIS], ["V24", "N", 30]],
+    [["V31", "O", 18-TAM_ROBO_DIVIDIDO_POR_DOIS]],
+]
+        
+    
+    assert grafo.matriz_obstaculos == matriz_esperada
+
+
+def test_desmarcar_obstaculo():
+    grafo = Grafo(matriz_lista_adjacencia, matriz_obstaculos)
+    grafo.marcar_obstaculo("V5")
+    grafo.desmarcar_obstaculo("V5")
+    assert grafo.matriz_obstaculos == grafo.matriz_adj
